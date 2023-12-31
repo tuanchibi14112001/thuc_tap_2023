b0a42dbda1c70a508b03f3212c81afaacb98b95c
tuanchibi14112001 <tuanchibi14112001@gmail.com>
2023-12-31 18:08:47 +0700

    add UI Splash and Login Screen

--- ChatApp/app/src/main/java/com/example/chatapp/MainActivity.kt
+++ ChatApp/app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import com.example.chatapp.databinding.ActivityMainBinding
 import com.google.firebase.database.DatabaseReference
@@ -11,20 +12,12 @@ import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var firebaseRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        firebaseRef = FirebaseDatabase.getInstance().getReference("test")
-
-        binding.btnSendData.setOnClickListener{
-            firebaseRef.setValue("Hello")
-                .addOnCompleteListener{
-                    Toast.makeText(this, "stored successfully", Toast.LENGTH_SHORT).show()
-                }
-        }
     }
 }
---
+++ ChatApp/app/src/main/java/com/example/chatapp/ui/splash/SplashScreenFragment.kt
@@ -0,0 +1,58 @@
+package com.example.chatapp.ui.splash
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.chatapp.R
+import com.example.chatapp.databinding.FragmentSplashScreenBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SplashScreenFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+@SuppressLint("CustomSplashScreen")
+class SplashScreenFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSplashScreenBinding.inflate(inflater, container, false)
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(3000)
+            test()
+        }
+    }
+
+    suspend fun test(){
+        findNavController().navigate(R.id.action_splashScreenFragment_to_loginFragment)
+    }
+
+
+}
