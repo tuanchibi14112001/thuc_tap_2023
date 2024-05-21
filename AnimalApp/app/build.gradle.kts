49550e5bfa02d9c7d59796a0a93fe39a0f9a6b29
tuanchibi14112001 <tuanchibi14112001@gmail.com>
2024-05-21 18:06:03 +0700

    add image picker

--- AnimalApp/app/build.gradle.kts
+++ AnimalApp/app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.6")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.6")
@@ -84,6 +84,11 @@ dependencies {
     // Recycler View
     implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.recyclerview:recyclerview-selection:1.1.0")
+
+    //Image Picker
+    implementation ("com.github.dhaval2404:imagepicker:2.1")
+    implementation ("androidx.activity:activity-ktx:1.9.0")
+    implementation ("androidx.fragment:fragment-ktx:1.7.1")
 
 }
 
--- AnimalApp/app/src/main/java/com/example/animalapp/ui/image_predict/ImagePredictFragment.kt
+++ AnimalApp/app/src/main/java/com/example/animalapp/ui/image_predict/ImagePredictFragment.kt
@@ -1,5 +1,7 @@
 package com.example.animalapp.ui.image_predict
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,6 +9,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
@@ -14,30 +18,57 @@ import com.example.animalapp.R
 import com.example.animalapp.base.BaseFragment
 import com.example.animalapp.databinding.FragmentImagePredictBinding
 import com.example.animalapp.utils.Status
+import com.github.dhaval2404.imagepicker.ImagePicker
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class ImagePredictFragment : BaseFragment<FragmentImagePredictBinding>() {
     private val viewModel: ImagePredictViewModel by viewModels()
+    private val startForProfileImageResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            val resultCode = result.resultCode
+            val data = result.data
+
+            if (resultCode == Activity.RESULT_OK) {
+                //Image Uri will not be null for RESULT_OK
+                val fileUri = data?.data!!
+
+                val mProfileUri = fileUri
+                binding.imgPic.setImageURI(fileUri)
+
+            } else if (resultCode == ImagePicker.RESULT_ERROR) {
+                Toast.makeText(requireContext(), ImagePicker.getError(data), Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(requireContext(), "Task Cancelled", Toast.LENGTH_SHORT).show()
+            }
+        }
+
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ) = FragmentImagePredictBinding.inflate(inflater, container, false)
 
     override fun prepareView(savedInstanceState: Bundle?) {
-        binding.btnNext.setOnClickListener{
-            findNavController().navigate(R.id.action_imagePredictFragment_to_loginFragment)
+        binding.btnTakePic.setOnClickListener {
+            ImagePicker.with(this)
+                .compress(1024)         //Final image size will be less than 1 MB(Optional)
+                .maxResultSize(
+                    1080,
+                    1080
+                )
+                .createIntent { intent ->
+                    startForProfileImageResult.launch(intent)
+                }
         }
-        viewModel.getTest()
-        observeModel()
     }
+
 
     private fun observeModel() {
         viewModel.dataFlow.observe(viewLifecycleOwner) {
             when (it.status) {
                 Status.SUCCESS -> {
-                    binding.btnNext.text = it.data?.detail
                     Log.d("CHECK", it.data.toString())
                     hideLoading()
                 }
