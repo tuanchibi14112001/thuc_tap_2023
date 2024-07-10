871954575468971808ff418015ea9d82d9987a39
tuanchibi14112001 <tuanchibi14112001@gmail.com>
2024-07-10 08:10:41 +0700

    fix

--- AnimalApp/app/src/main/java/com/example/animalapp/utils/Constants.kt
+++ AnimalApp/app/src/main/java/com/example/animalapp/utils/Constants.kt
@@ -1,7 +1,8 @@
 package com.example.animalapp.utils
 
 object Constants {
-    const val BASE_URL =  "http://192.168.1.6:8000/api/"
+    //    const val BASE_URL =  "http://192.168.1.6:8000/api/"
+    const val BASE_URL = "http://192.168.43.119:8000/api/"
     const val BASE_URL_AI = "https://sole-allowing-hugely.ngrok-free.app"
     const val END_POINT = "shows"
 }
